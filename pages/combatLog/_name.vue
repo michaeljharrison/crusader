413f28c57778b685d2bef62c11377ee8146f85d1
michaeljharrison <michael.j.harrison@outlook.com>
2021-02-23 08:19:37 +1100

    Redirect fix

--- pages/combatLog/_name.vue
+++ pages/combatLog/_name.vue
@@ -359,7 +359,7 @@ export default {
             })
             this.$message.success(`Battle Report Updated.`)
             setTimeout(() => {
-              window.location.href = this.name || 'combatLog'
+              window.location.href = 'combatLog'
             }, 0)
           } catch (e) {
             console.error(e)
